0df25dc0d6ede36bb1c694c42f0568c52293593a
Nikhil Sha <nikhil.sha@outlook.com>
2023-12-14 21:50:13 -0500

    README and basic infra.

--- mediapipe_stuff/examples/face_landmarker/raspberry_pi/setup.sh
+++
@@ -1,5 +0,0 @@
-# Install Python dependencies.
-python3 -m pip install pip --upgrade
-python3 -m pip install -r requirements.txt
-
-wget -O face_landmarker.task -q https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task
